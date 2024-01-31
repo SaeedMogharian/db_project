b96872595942dcabfa53bf141ea6a936ca77619f
Saeed Mogharian <moghariansaeed@gmail.com>
2024-01-31 18:51:24 +0330

    primary models definition

--- src/AcAdDB/student/models.py
+++ src/AcAdDB/student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from core.models import UserAccount
+
+class Student(UserAccount):
+    student_id = models.CharField(max_length=15, unique=True)
+    
+    # Add other student-related fields
+
+# Add more student-related models as needed
